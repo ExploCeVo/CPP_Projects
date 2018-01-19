6017c6758bce2a6e326ec33b08fa8d10e9f339c9
Cole Chapin <cchapin15@gmail.com>
2018-01-19 14:05:51 -0600

    EH, most of this will prolly get deleted, fixing junk
    /title

---
+++ Chapter5/Chapter5/Chapter5.cpp
@@ -0,0 +1,41 @@
+/*
+Code examples and notes from chapter 5 of Programming Practices and Principles.
+
+CHAPTER 5 - ERRORS
+====================================================================================
+Common types of errors
+====================================================================================
+		Type								Definition
+Poor Specification		Not specific about what a program should do. Cover cases.
+Incomplete Program		Not all cases have been taken care of yet.
+Unexpected Args			Occurs when a function is given an argument it doesn't use.
+Unexpected Input		Typically when reading input from outer sources.
+Unexpected State		Various needed data is incorrect for program to run.
+Logical Error			Code that is just wrong.
+====================================================================================
+
+EXCEPTIONS
+If a function finds an error that it cannot handle it does not return normally.
+Instead, it throws an exception indicating something went wrong. 
+Use this using the "throws" syntax.
+
+DEBUGGING
+1. Get program to compile.
+2. Get the program to link.
+3. Get the program to do what it is supposed to do.
+4. Do this millions of times til you get it right.
+5. ???
+6. Neck.
+7. There is never the last bug.
+
+Legit nothing worth coding in this chapter.
+*/
+#include "stdafx.h"
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ Project6/Project6/Project6.cpp
@@ -0,0 +1,16 @@
+/*
+Chapter 6 examples and notes from Programming Practices and Principles.
+==============================================================================
+CHAPTER 6: WRITING A PROGRAM
+==============================================================================
+
+*/
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
