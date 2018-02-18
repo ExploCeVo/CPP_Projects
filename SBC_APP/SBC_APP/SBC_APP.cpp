c108132ba025e765104b77e20e402ca74e13e974
Cole Chapin <cchapin15@gmail.com>
2018-02-18 15:24:20 -0600

    Starting Over: 2/18/18
    Gutted the original implementation.
    Data Handling should now be good, There isn't much of a working
    implementation as far as the console goes yet.
    Most of what needs to be worked on is print out statements for now.

--- SBC_APP/SBC_APP/SBC_APP.cpp
+++ SBC_APP/SBC_APP/SBC_APP.cpp
@@ -1,36 +1,45 @@
 /*
 SBC_APP.cpp
 
-This application is intended to do the following:
- 1. Take in a list of players and their estimated prices
- 2. Store this list and display them until purchased on fifa 
- 3. As players are purchased, user can enter amounts paid until entire team is bought
- 4. Display total cost for purchased team
- 5. Compare the total to the estimate as well as estimated worth of the packs earned
-	from completing the certain sbc.
- 6. yadayada
+ ==========================================================================================
+ Grammer, currently a work in progress:
 
- il finish this bullshit later..
+ Statement:
+	Create List
+	Add
+	View List
+	Quit
+View List:
+	"View"
+Quit:
+	"Quit"
+Create List:
+	List_Name
+	"Create"
+Add:
+	Player
+	Price
+Player:
+	String
+Price:
+	floating-point-literal
+List_Name:
+	String
  ==========================================================================================
  CURRENT IMPLEMENTATIONS:
-	prompt(); asks for a user prompt, and returns the proper int value according to answer.
-	test(); function that runs everything currently being tested.
-	isFloat(string myString); function checks if user input is a float.
-	createList(); function that creates a list of players and prices, storing them in a
-	text file.
-	lookupList(); function opens a file and lists players and prices found.
+	The players vector should be more or less working properly, will be easier to test once
+	some work is done as far as grabbing examples from the console.
 
-	Added Variable, Token, and Token_stream classes to help with finding and eliminating
-	errors.
-	get() function needs some work, whether adding token checks or deleting some.
+	Implementation of Token_stream::get() needs some work
+
+	Functionality of the outer parts of the program need some work
  ==========================================================================================
 	TODO:
-	Change from using a text file to store information to using the vector var_table. TODO
-	Add functionality for printing current var_table as well as editing it. TODO
-	Edit prompts to work with the new functionality. TODO
-	WERE GETTING SOMEWHERE... KINDA
+	Implement the current grammar and test it
+
+	Implement Outer functionality of the program
  ==========================================================================================
- last edit: 2/13/18
+ last edit: 2/18/18
  author: Cole Chapin
  ==========================================================================================
 */
@@ -38,15 +47,18 @@ This application is intended to do the following:
 #include "stdafx.h"
 #include "std_lib_facilities.h"
 
-// classes created for use in this program
-class Variable {
+// player struct
+struct Player {
 public:
 	string name;
 	double value;
+
+	// initialization
+	Player(string s, double v) : name(s), value(v) {}
 };
 
-// class definitions
-class Token
+// token struct
+struct Token
 {
 public:
 	char kind;
@@ -59,155 +71,118 @@ public:
 	Token(char ch, string n) : kind{ch}, name{n}{}
 };
 
+// Token_stream class
 class Token_stream {
-public:
-	Token get();
-	void putback(Token t);
-private:
-	bool full{ false };
+	bool full;
 	Token buffer;
+public:
+	Token_stream() : full(0), buffer(0) {}
+
+	Token get();
+
+	void unget(Token t) { buffer = t; full = true; }
+
+	void ignore(char);
 };
 
-// constant definitions and variables
-const string FileName = "playerdata.txt";
-const char name = 'N';
-const char number = '8';
-const char add = 'A';
-const string decakey = "add";
+vector<Player> players;
 Token_stream ts;
-vector<Variable> plr_table;
-
-// declarations used by Token_stream
 
 // puts token in buffer and marks it full
-void Token_stream::putback(Token t)
+void Token_stream::unget(Token t)
 {
 	buffer = t;
 	full = true;
 }
 
+// grabs next token from buffer
 Token Token_stream::get()
 {
+	//TODO
+}
 
-	if (full) {	// check if we have already have a token ready
+// ignores char c in the token stream
+void Token_stream::ignore(char c)
+{
+	if (full && c == buffer.kind) {
 		full = false;
-		return buffer;
+		return;
 	}
+	full = false;
 
 	char ch;
-	cin >> ch;	// >> skips whitespace like space, newline, tab, etc.
-
-	switch (ch) {
-	case ';':	// for "print"
-	case 'q':	// for "quit"
-	case '(':
-	case ')':
-	case '+':
-	case '-':
-	case '*':
-	case '/':
-	case '%':
-		return Token{ ch };	// let each character represent itself
-	case '.':
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9':
-	{
-		cin.putback(ch);
-		double val;
-		cin >> val;
-		return Token{ number, val };
-	}
-	default:
-		if (isalpha(ch)) {
-			cin.putback(ch);
-			string s;
-			s += ch;
-			while (cin.get(ch) && (isalpha(ch) || isdigit(ch)))
-				s += ch;
-			if (s == decakey) return Token{ add };
-			return Token{ name, s };
-		}
-		error("Bad Token");
-	}
-}
-
-// declarations used by var_table
-double get_price(string s);
-void set_price(string s, double d);
-bool in_list(string var);
-double add_player(string var, double val);
-
-// declarations not used by var_table
-int prompt();
-void test();
-bool isFloat(string myString);
-
-// main hehe
-void main()
-{
-	test();
-	return;
-} 
-
-// function to test if entered string could be converted to a float
-bool isFloat(string myString) {
-	std::istringstream iss(myString);
-	float f;
-	iss >> noskipws >> f; // noskipws considers leading whitespace invalid
-						  // Check the entire string was consumed and if either failbit or badbit is set
-	return iss.eof() && !iss.fail();
-}
-
-// calls prompt()
-void test()
-{
-	int ans = prompt();
-		error("Invalid Entry.\n");
-}
-
-// initial prompt for app
-int prompt()
-{
-	return 0;
+	while (cin >> ch)
+		if (ch == c)
+			return;
 }
 
 /*
 ==========================================================================================
-	FUNCTIONS USED BY VAR_TABLE
+Helper Functions for data struct
 ==========================================================================================
-*/
+ */
 
-// return the price of the player named plr
-double get_price(string plr)
+double get_cost(string nm)
 {
-	for (const Variable& v : var_table)
-		if (v.name == plr) return v.value;
-	error("get: Undefined Variable", plr);
+	for(int i = 0; i < players.size; i++)
+		if (players[i].name == nm) 
+			return players[i].value;
+	error(nm, "Is not in the current list.\n");
 }
 
-// set the player plr price to d
-void set_price(string plr, double d)
+// sets the cost of player nm to cst if player is found in the list, otherwise an 
+// error is thrown. This helper method might not be necessary
+void set_cost(string nm, double cst)
 {
-	for(Variable& v: var_table)
-		if (v.name == plr) {
-			v.value = d;
+	for(int i = 0; i < players.size; i++)
+		if (players[i].name == nm) {
+			players[i].value == cst;
 			return;
 		}
-	error("Set: undefined Variable.", s);
+	error("Player not found in the list: ", nm);
 }
 
-// check if plr is already in var_table
-bool in_list(string plr)
+// adds player to list, if player is already found in list, throws an error
+void add_player(string nm, double cst)
 {
-	for (const Variable& v : var_table)
-		if (v.name == plr) return true;
+	for (int i = 0; i < players.size; i++)
+		if (players[i].name == nm)
+			error(nm, "Is already in the list.\n");
+	players.push_back(Player{ nm, cst });
+	return;
+}
+
+// returns true if player is in list, false otherwise
+bool in_table(string nm)
+{
+	for (int i = 0; i < players.size(); ++i)
+		if (players[i].name == nm)
+			return true;
 	return false;
 }
 
-// add {plr, val} to plr_table
-double add_player(string plr, double val)
+// Prints the full list of players to the console
+void print_list()
 {
-	if (in_list(plr)) error(plr, "declared twice");
-	var_table.push_back(Variable{ plr, val });
-	return val;
+	if (players.size < 0)
+		error("List is Empty");
+	for (int i = 0; i < players.size; i++)
+		cout << players[i].name << ": " << players[i].value << endl;
+	return;
+}
+
+/*
+==========================================================================================
+Other Functions
+==========================================================================================
+*/
+
+void display()
+{
+	//TODO
+}
+
+int main() 
+{
+	//TODO
 }
